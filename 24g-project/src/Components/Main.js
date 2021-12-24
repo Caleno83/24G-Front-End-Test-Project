39061b493c13f30ea75b12cf78b0fa7011415924
Jorge Jimenez <jorgejimenez@Jorges-MacBook-Air-2.local>
2021-12-24 10:49:24 -0500

    added paragraph text into section tag, and added its box size, and styling. fixed width in index.css

--- 24g-project/src/Components/Main.js
+++ 24g-project/src/Components/Main.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
-    return (
-        <main>
-            <h1>Welcome Holiday Text</h1>
-        </main>
-    )
-}
+  return (
+    <main>
+      <h1>Welcome Holiday Text</h1>
+      <section>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus
+          odio ac ante malesuada feugiat. Mauris pretium, nulla ac imperdiet
+          suscipit, nibh enim pellentesque sapien, ut hendrerit dolor sapien
+          gravida lacus.
+        </p>
+      </section>
+    </main>
+  );
+};
 
-export default Main
+export default Main;
