2b0f39dc50f0036f67fcad2a7a59884413fc7ce6
Jorge Jimenez <jorgejimenez@Jorges-MacBook-Air-2.local>
2021-12-24 10:30:35 -0500

    created main component, and in there added h1 tag with main writing, and box sizes and styling specs.  Added link for styling to html, and changed tab name there too.

--- 24g-project/src/App.js
+++ 24g-project/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.scss';
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
   return (
     <body>
       <Header />
+      <Main />
     </body>
   );
 }
---
+++ 24g-project/src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+            <h1>Welcome Holiday Text</h1>
+        </main>
+    )
+}
+
+export default Main
