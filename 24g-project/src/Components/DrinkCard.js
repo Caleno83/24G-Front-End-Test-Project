a2df993dcceda1c9bc97c19dca3d50327ce2c57a
Jorge Jimenez <jorgejimenez@Jorges-MacBook-Air-2.local>
2021-12-26 14:55:14 -0500

    Created modal for opening and closing new component with information about drinks with border sizes specs, styling, and booleans that when clicking the modal open and close

--- 24g-project/src/Components/DrinkCard.js
+++ 24g-project/src/Components/DrinkCard.js
@@ -1,15 +1,47 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import ModalInfo from "./ModalInfo";
+import Modal from "react-modal";
 
 const DrinkCard = ({ item }) => {
   const [hover, setHover] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.style.overflow = modalOpen ? "hidden" : "auto";
+  }, [modalOpen]);
 
   return (
-    <div
-      className="images-front"
-      onMouseOver={() => setHover(true)}
-      onMouseOut={() => setHover(false)}
-    >
-      <img src={hover ? item.images.back : item.images.front} alt="drink recipes images" />
+    <div>
+      <div
+        className="images-front"
+        onMouseOver={() => setHover(true)}
+        onMouseOut={() => setHover(false)}
+        onClick={() => setModalOpen(true)}
+      >
+        <img
+          src={hover ? item.images.back : item.images.front}
+          alt="drink recipes images"
+        />
+      </div>
+      <div className="modal-box">
+        <Modal
+          className="mymodal"
+          overlayClassName="overlay"
+          isOpen={modalOpen}
+        >
+          <div className="image-logo">
+            <img
+              onClick={() => setModalOpen(false)}
+              src=" https://assets.24g.com/public/2022-frontend-test-project/exit_icon.svg"
+              alt="Close Logo"
+            />
+          </div>
+          <div>
+            <ModalInfo item={item} />
+          </div>
+        </Modal>
+      </div>
     </div>
   );
 };
